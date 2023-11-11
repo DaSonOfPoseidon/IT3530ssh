a91c05ae007140cd9790883b17b29983c4e684cd
DaSonOfPoseidon <106688879+DaSonOfPoseidon@users.noreply.github.com>
2023-11-10 18:13:01 -0600

    Ascii & .exe

--- MyAscii.c
+++ MyAscii.c
@@ -4,7 +4,7 @@ int main() {
     printf("Enter a character: ");
     scanf("%c", &c);  
     
-    printf("ASCII value of %c = %d", c, c);
+    printf("ASCII value of %c = %d\n", c, c);
     
     return 0;
 }
